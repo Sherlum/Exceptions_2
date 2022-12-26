c8cd2bcfc29bb1736fc6bec4889fe4498a2196eb
Stanislav <hansolo99@yandex.ru>
2022-12-26 11:21:48 +0300

    add Readme.txt

--- Task2.java
+++ Task2.java
@@ -1,7 +1,5 @@
-// Если необходимо, исправьте данный код (задание 2 https://docs.google.com/document/d/17EaA1lDxzD5YigQ5OAal60fOFKVoCbEJqooB9XfhT7w/edit)
-// Дан следующий код, исправьте его там, где требуется (задание 3 https://docs.google.com/document/d/17EaA1lDxzD5YigQ5OAal60fOFKVoCbEJqooB9XfhT7w/edit)
-
 // package Sem2;
+
 public class Task2 {
     public static void main(String[] args) {
         foo1();
